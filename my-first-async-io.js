09b6796b0e2480d42d8d692c0594c5ad628510d4
John Manack <jmnck28@gmail.com>
2021-01-27 08:53:19 -0500

    adds async io exercise

--- my-first-async-io.js
+++ my-first-async-io.js
@@ -1,4 +1,8 @@
 'use strict';
 const fs = require("fs");
 
-const buf = fs.readFile
+fs.readFile(process.argv[2], 'utf8', (error, data) => {
+    if (error) console.log(error.message);
+    console.log(data.split('\n').length -1)
+    }
+);
